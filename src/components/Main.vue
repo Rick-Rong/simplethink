8e08957e3795d678bfe28459937aeb5cfad194a2
rsq <rongshuqiang>
2019-11-18 18:14:21 +0800

    ...

---
+++ src/components/Main.vue
@@ -0,0 +1,35 @@
+<template>
+	<span :style="initStyle(bgStyle)">{{msg}}</span>
+</template>
+
+<script>
+export default{
+	name:'Main',
+	data(){
+		return{
+			msg:'hello!',
+			bgStyle:{
+				height:200,
+				width:200,
+				color:'blue'
+			}
+		}
+	},
+	methods:{
+		initStyle:(bgStyle)=>{
+			let styleObj = {};
+			for(let key in bgStyle){
+				if(key=='height'||key=='width'||key=='top'||key=='left'){
+					styleObj[key] = bgStyle[key]+'px';
+				}else{
+					styleObj[key] = bgStyle[key];
+				}
+			}
+			return styleObj;
+		}
+	}
+}
+</script>
+
+<style>
+</style>
